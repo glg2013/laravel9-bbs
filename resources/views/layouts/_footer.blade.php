45739dd89ec574e920af1504840625ba743357a3
glg2013 <57412960@qq.com>
2023-05-06 06:19:36 +0000

    基础页面结构

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-start">
+      由 <a href="https://weibo.com/u/1676952407?is_hot=1" target="_blank">疯狂的马铃薯</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+    </p>
+
+    <p class="float-end"><a href="mailto:name@email.com">联系我们</a></p>
+  </div>
+</footer>
