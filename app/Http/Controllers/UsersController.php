3a0ae3eb1e3d94166e7e68c1b4eb7ccf959e2d04
glg2013 <57412960@qq.com>
2023-05-18 07:30:09 +0000

    裁剪头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -24,7 +24,7 @@ class UsersController extends Controller
         $data = $userRequest->all();
 
         if ($userRequest->file('avatar')) {
-            $result = $uploader->save($userRequest->file('avatar'), 'avatars', $user->id);
+            $result = $uploader->save($userRequest->file('avatar'), 'avatars', $user->id, 416);
             if ($result) {
                 $data['avatar'] = $result['path'];
             }
