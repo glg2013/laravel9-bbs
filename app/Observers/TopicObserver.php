c04d79df19983e42dc5421cf05f6ea33f9e2319e
glg2013 <57412960@qq.com>
2023-05-24 07:34:31 +0000

    翻译队列化

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
 use App\Models\Topic;
 
 // creating, created, updating, updated, saving,
@@ -28,12 +29,21 @@ class TopicObserver
         // 生成话题摘录
         $topic->excerpt = make_excerpt($topic->body);
 
-        // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
-        if ( ! $topic->slug) {
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
-        }
-
         //$topic->setAttribute('body', clean($topic->getAttribute('body'), 'user_topic_body'));
         //$topic->setAttribute('excerpt', make_excerpt($topic->getAttribute('body')));
+    }
+
+    public function saved(Topic $topic)
+    {
+        // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
+        if ( ! $topic->slug) {
+
+            // 使用队列的异步方式
+            // 推送翻译 Slug 的任务到队列
+            dispatch(new TranslateSlug($topic));
+
+            // 同步的方式会因为网络请求的不确定性而产生隐患，弃用了
+            //$topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+        }
     }
 }
