5ef1eab86280755fe06bf2b1f1106afa4411ce40
glg2013 <57412960@qq.com>
2023-05-24 06:07:04 +0000

    用户可以编辑话题

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -44,21 +44,22 @@ class TopicsController extends Controller
         $topic->setAttribute('user_id', \Auth::id());
 		$topic->save();
 
-		return redirect()->route('topics.show', $topic->getAttribute('id'))->with('message', '帖子创建成功！');
+		return redirect()->route('topics.show', $topic->getAttribute('id'))->with('success', '帖子创建成功！');
 	}
 
 	public function edit(Topic $topic)
 	{
         $this->authorize('update', $topic);
-		return view('topics.create_and_edit', compact('topic'));
+        $categories =   Category::all();
+		return view('topics.create_and_edit', compact('topic', 'categories'));
 	}
 
 	public function update(TopicRequest $request, Topic $topic)
 	{
-		$this->authorize('update', $topic);
-		$topic->update($request->all());
+        $this->authorize('update', $topic);
+        $topic->update($request->all());
 
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
+        return redirect()->route('topics.show', $topic->id)->with('success', '更新成功！');
 	}
 
 	public function destroy(Topic $topic)
